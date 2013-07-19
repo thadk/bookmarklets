07412d147657a8beaceed8f2c36b46e4ed282397
James Campos <james.r.campos@gmail.com>
2013-07-19 02:54:17 -0700

    meta

---
+++ README.md
@@ -0,0 +1,4 @@
+# bookmarklets
+
+currently only `github.js`, which redirects you to a repo's github.io page.
+also see [rawgithub](https://github.com/rgrove/rawgithub)
--- github.js
+++ github.js
@@ -1,4 +1,4 @@
 javascript:(function() {
   var split = location.pathname.slice(1).split('/');
   window.location = 'http://' + split[0] + '.github.io/' + split[1];
-})()
+})();
