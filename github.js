39ae5db704f0c19da3b1e1496afe85ef912576ee
Thad Kerosky <contact@thadk.net>
2015-04-03 13:56:21 -0400

    add AMP toggles

--- github.js
+++ github.js
@@ -1,8 +1,26 @@
 javascript:(function() {
   var host = location.host.split('.');
   var path = location.pathname.slice(1).split('/');
-
-  window.location = host[2] === 'io'
+  
+  var winlocation = '' + window.location;
+  
+  var isGISPath = /(gisModule\/)(dev\/app|dist)(\/)/;
+  var isDashboardPath = /(dashboard\/)(dev\/app|build)(\/)/;
+  
+  if (isGISPath.exec(location.pathname)) {
+  window.location = isGISPath.exec(location.pathname)[2] == 'dist'
+    ? winlocation.replace(isGISPath,"$1dev/app$3")
+    : winlocation.replace(isGISPath,"$1dist$3");
+  }
+    
+  else if (isDashboardPath.exec(location.pathname)) {
+    window.location = isDashboardPath.exec(location.pathname)[2] == 'build'
+    ? winlocation.replace(isDashboardPath,"$1dev/app$3")
+    : winlocation.replace(isDashboardPath,"$1build$3")
+  }
+  else {
+    window.location = host[2] === 'io'
     ? 'https://github.com/' + host[0] + '/' + path[0]
     : 'http://' + path[0] + '.github.io/' + path[1];
+  }
 })();
